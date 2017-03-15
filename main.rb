3428bd984fc72ac7803ae2f5478cb04c08a4e85e
Cam Crews <camcrews@gmail.com>
2017-03-15 11:23:56 -0400

    Sinatra API example

--- main.rb
+++ main.rb
@@ -3,24 +3,42 @@
 
 require 'net/http'
 require 'json'
+require 'sinatra'
 
 base_uri = 'https://itunes.apple.com/search'
 
-puts "Enter an artist"
-artist = gets.strip
+# puts "Enter an artist"
+# artist = gets.strip
 
-response = Net::HTTP.get(URI("#{base_uri}?term=#{artist}&entity=song"))
 
-results = JSON.parse( response )
-
-$data = results['results'].map do |song|
-  {
-    id: song['trackId'],
-    name: song['trackName'],
-    price: song['trackPrice'],
-    picture: song['artworkUrl100']
-  }
+# provide a form to search for an artist
+get '/' do
+  @artist = nil
+  @results = nil
+  erb :home
 end
+
+# output the results of the search
+post '/' do
+  @artist = params[:artist]
+
+  response = Net::HTTP.get(URI("#{base_uri}?term=#{@artist}&entity=song"))
+
+  results = JSON.parse( response )
+
+  @results = results['results'].map do |song|
+    {
+      id: song['trackId'],
+      name: song['trackName'],
+      price: song['trackPrice'],
+      picture: song['artworkUrl100']
+    }
+  end
+
+  erb :home
+end
+
+
 
 
 
